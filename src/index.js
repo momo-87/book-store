290fbf76e2867b12e9f398c386b4512f3196d3a8
Momo-87 <momochristian87@gmail.com>
2023-06-08 12:27:44 +0100

    Set React Router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import BookStoreApp from 'BookStoreApp';
@@ -5,6 +6,8 @@ import BookStoreApp from 'BookStoreApp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BookStoreApp />
+    <Router>
+      <BookStoreApp />
+    </Router>
   </React.StrictMode>,
 );
