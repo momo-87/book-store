d3e7286135a3e7fcd51ab591c3dfdc9d70294b3b
Momo-87 <momochristian87@gmail.com>
2023-06-08 11:39:51 +0100

    Clean up the workspace

---
+++ src/BookStoreApp.js
@@ -0,0 +1,13 @@
+function BookStoreApp() {
+  return (
+    <div>
+      <header>
+        <p>
+          Welcome to BooksStore React App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default BookStoreApp;
