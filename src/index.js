3629d163a6804220fd88070ec92b45eed038b144
Momo-87 <momochristian87@gmail.com>
2023-06-08 12:17:22 +0100

    Set support for absolute import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BookStoreApp from './BookStoreApp';
+import BookStoreApp from 'BookStoreApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
