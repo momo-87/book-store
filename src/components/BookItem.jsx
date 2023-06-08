5ea9bc68b2bb3e2c8bc335529be6c59b79bb7c35
Momo-87 <momochristian87@gmail.com>
2023-06-09 00:11:25 +0100

    Create BooksLogic.jsx component, BookItem.jsx  for individual book state and BooksList.jsx for book list

---
+++ src/components/BookItem.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+
+const BookItem = ({ book, delBook }) => (
+  <li>
+    <div>
+      <div>
+        <span>{book.category}</span>
+        <h3>{book.title}</h3>
+        <span>{book.author}</span>
+      </div>
+      <div>
+        <button type="button">
+          Comments
+        </button>
+        <button type="button" onClick={() => delBook(book.id)}>
+          Remove
+        </button>
+        <button type="button">
+          Edit
+        </button>
+      </div>
+    </div>
+    <div>
+      {book.progress}
+    </div>
+    <div>
+      <span>CURRENT CHAPTER</span>
+      <span>
+        CHAPTER
+        {book.currentChapter}
+      </span>
+      <button type="button">UPDATE PROGRESS</button>
+    </div>
+  </li>
+);
+export default BookItem;
+
+BookItem.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.string,
+    category: PropTypes.string,
+    title: PropTypes.string,
+    author: PropTypes.string,
+    progress: PropTypes.string,
+    currentChapter: PropTypes.number,
+  }).isRequired,
+  delBook: PropTypes.func.isRequired,
+};
---
+++ src/components/BooksLogics.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import BooksList from 'components/BooksList';
+
+const Book = () => {
+  const [books, setBooks] = useState([{
+    id: uuidv4(), category: 'Action', title: 'The Hunter Games', author: 'Suzanne Collins', progress: '64%', currentChapter: '17',
+  }]);
+
+  const handleDelete = (id) => {
+    setBooks([...books.filter((book) => book.id !== id)]);
+  };
+
+  return (
+    <div>
+      <BooksList delBook={handleDelete} books={books} />
+    </div>
+  );
+};
+export default Book;
