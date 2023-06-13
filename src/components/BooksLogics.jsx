298f62dabe46432881b3116f089b27593373dbcc
Momo-87 <momochristian87@gmail.com>
2023-06-13 07:17:08 +0100

    Add bookItems.js file to store hard coded boos items

--- src/components/BooksLogics.jsx
+++ src/components/BooksLogics.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import BooksList from 'components/BooksList';
 import BookInput from 'components/BookInput';
+import bookItems from 'redux/books/bookItems';
 import 'styles/bookListAndInput.scss';
 
 const BooksLogics = () => {
-  const [books, setBooks] = useState([{
-    id: uuidv4(), category: 'Action', title: 'The Hunter Games', author: 'Suzanne Collins', progress: '100%', currentChapter: '17',
-  }]);
+  const [books, setBooks] = useState(bookItems);
 
   const handleDelete = (id) => {
     setBooks([...books.filter((book) => book.id !== id)]);
