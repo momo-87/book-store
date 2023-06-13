91bdcd2847b67d95c3967b3fdaee8eb2d73d3ca4
Momo-87 <momochristian87@gmail.com>
2023-06-13 09:25:21 +0100

    Add a reducer that adds book fixing bugs and rendering the book store

--- src/components/BookInput.jsx
+++ src/components/BookInput.jsx
@@ -1,36 +1,40 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import { addBook } from 'redux/books/bookSlice';
+// import { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { v4 as uuidv4 } from 'uuid';
+// import { addBook } from 'redux/books/bookSlice';
+import PropTypes from 'prop-types';
 
-const BookInput = () => {
-  const [author, setAuthor] = useState('');
-  const [title, setTitle] = useState('');
-  const dispatch = useDispatch();
-  const handleAuthor = (e) => (setAuthor(e.target.value));
-  const handleTitle = (e) => (setTitle(e.target.value));
+const BookInput = ({ handleTitle, handleAuthor, handleAddBtn }) => (
+  // const [author, setAuthor] = useState('');
+  // const [title, setTitle] = useState('');
+  // const dispatch = useDispatch();
+  // const handleAuthor = (e) => (setAuthor(e.target.value));
+  // const handleTitle = (e) => (setTitle(e.target.value));
 
-  const handleAddBtn = () => {
-    const bookTitle = title.trim();
-    const bookAuthor = author.trim();
-    if (bookTitle && bookAuthor) {
-      const newBook = {
-        id: uuidv4(),
-        title: bookTitle,
-        author: bookAuthor,
-      };
-      dispatch(addBook(newBook));
-    }
-  };
+  // const handleAddBtn = () => {
+  //   const bookTitle = title.trim();
+  //   const bookAuthor = author.trim();
+  //   if (bookTitle && bookAuthor) {
+  //     const newBook = {
+  //       id: uuidv4(),
+  //       title: bookTitle,
+  //       author: bookAuthor,
+  //     };
+  //     dispatch(addBook(newBook));
+  //   }
+  // };
 
-  return (
-    <form>
-      <span>ADD NEW BOOK</span>
-      <input type="text" placeholder="Book title" onChange={handleTitle} />
-      <input type="text" placeholder="Author" onChange={handleAuthor} />
-      <button type="button" onClick={handleAddBtn}>ADD BOOK</button>
-    </form>
-  );
-};
-
+  <form>
+    <span>ADD NEW BOOK</span>
+    <input type="text" placeholder="Book title" onChange={handleTitle} />
+    <input type="text" placeholder="Author" onChange={handleAuthor} />
+    <button type="button" onClick={handleAddBtn}>ADD BOOK</button>
+  </form>
+);
 export default BookInput;
+
+BookInput.propTypes = {
+  handleTitle: PropTypes.func.isRequired,
+  handleAuthor: PropTypes.func.isRequired,
+  handleAddBtn: PropTypes.func.isRequired,
+};
--- src/components/BooksLogics.jsx
+++ src/components/BooksLogics.jsx
@@ -1,20 +1,49 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import BooksList from 'components/BooksList';
 import BookInput from 'components/BookInput';
 import bookItems from 'redux/books/bookItems';
+import { addBook } from 'redux/books/bookSlice';
 import 'styles/bookListAndInput.scss';
+import { useSelector, useDispatch } from 'react-redux';
 
 const BooksLogics = () => {
-  const [books, setBooks] = useState(bookItems);
-
+  // Get all books items (from Redux store and hard coded booksItems.js file)
+  const storedBooks = useSelector((state) => state.books);
+  const [books, setBooks] = useState([...storedBooks, ...bookItems]);
+  // Handle delete books
   const handleDelete = (id) => {
     setBooks([...books.filter((book) => book.id !== id)]);
+  };
+  // Get authon name and book title from inputs
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+  const handleAuthor = (e) => (setAuthor(e.target.value));
+  const handleTitle = (e) => (setTitle(e.target.value));
+  // Handle add book
+  const handleAddBtn = () => {
+    const bookTitle = title.trim();
+    const bookAuthor = author.trim();
+    if (bookTitle && bookAuthor) {
+      const newBook = {
+        id: uuidv4(),
+        title: bookTitle,
+        author: bookAuthor,
+      };
+      dispatch(addBook(newBook));
+      setBooks([...books, newBook]);
+    }
   };
 
   return (
     <div className="bookList-bookInput-box">
       <BooksList delBook={handleDelete} books={books} />
-      <BookInput />
+      <BookInput
+        handleAuthor={handleAuthor}
+        handleTitle={handleTitle}
+        handleAddBtn={handleAddBtn}
+      />
     </div>
   );
 };
