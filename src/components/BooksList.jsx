3b7b59f57114371ae548a13d92e44bffd9fb66fd
Momo-87 <momochristian87@gmail.com>
2023-06-15 00:39:21 +0100

    Fetch your books from the Bookstore API and rendering

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -1,15 +1,22 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 
-const BooksList = ({ books }) => (
-  <ul>
-    {[...books].map((book) => (
-      <BookItem book={book} key={book.item_id} />
-    ))}
-  </ul>
-);
+const BooksList = () => {
+  const { bookItems, isLoading, error } = useSelector((state) => state.books);
+  return (
+    <ul className="books-list">
+      { isLoading ? <p>Loading...</p> : undefined }
+      {error ? <p>{error}</p> : undefined}
+      {bookItems.map((book) => (
+        <BookItem key={book.item_id} book={book} />
+      ))}
+    </ul>
+  );
+};
+
 export default BooksList;
 
-BooksList.propTypes = {
-  books: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-};
+// BooksList.propTypes = {
+//   books: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+// };
--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,24 +1,62 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// export const addNewApp = createAsyncThunk('books/addNewAPP', async (rejectWithValue) => {
+//   try {
+//     const response = await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/');
+//     console.log(response.data);
+//     return response;
+//   } catch (err) {
+//     return rejectWithValue('Failed to create a new app');
+//   }
+// });
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async (appId = 'sMkThm8S72y7VovqTFP8', { rejectWithValue }) => {
+  try {
+    const response = await axios.get(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appId}/books`);
+    return response.data;
+  } catch (error) {
+    return rejectWithValue('Failed to fetch books data !!');
+  }
+});
+
+export const addNewBook = createAsyncThunk('books/addNewbook', async (book = {
+  item_id: 'item3',
+  title: 'The Selfish Gene',
+  author: 'Richard Dawkins',
+  category: 'Nonfiction',
+}) => {
+  try {
+    const response = await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/sMkThm8S72y7VovqTFP8/books', book);
+    return response.json();
+  } catch (err) {
+    return err.message;
+  }
+});
 
 const initialState = {
-  bookItems: [{
-    item_id: 'item1',
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item2',
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item3',
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-  }],
+  bookItems: [
+    // {
+    //   item_id: 'item1',
+    //   title: 'The Great Gatsby',
+    //   author: 'John Smith',
+    //   category: 'Fiction',
+    // },
+    // {
+    //   item_id: 'item2',
+    //   title: 'Anna Karenina',
+    //   author: 'Leo Tolstoy',
+    //   category: 'Fiction',
+    // },
+    // {
+    //   item_id: 'item3',
+    //   title: 'The Selfish Gene',
+    //   author: 'Richard Dawkins',
+    //   category: 'Nonfiction',
+    // }
+  ],
+  isLoading: true,
+  error: undefined,
 };
 
 const BookSlice = createSlice({
@@ -33,6 +71,37 @@ const BookSlice = createSlice({
     removeBook: (state, action) => {
       state.bookItems = state.bookItems.filter((book) => book.item_id !== action.payload);
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        // Get the action.payload keys values (item1 item2 item3,...)
+        //  which actually represent the books id)
+        const booksId = Object.keys(action.payload);
+        // Create bookItem object with the corresponding item_id
+        const books = [];
+        booksId.forEach((elt) => {
+          const bookItem = {
+            item_id: elt,
+            title: action.payload[elt][0].title,
+            author: action.payload[elt][0].author,
+            category: action.payload[elt][0].category,
+          };
+          books.push(bookItem);
+        });
+        state.bookItems = books;
+      })
+      .addCase(fetchBooks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(addNewBook.fulfilled, (action) => {
+        console.log(action.paylod);
+      });
   },
 });
 
